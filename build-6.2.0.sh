97bbd0221b522950f4fbbdb59538357016d0c88f
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2022-04-19 12:52:34 +0300

    Add build script for 6.2.0

---
+++ build-6.2.0.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+wget https://download.qemu.org/qemu-6.2.0.tar.xz
+tar xfv qemu-6.2.0.tar.xz
+rm -rfv qemu-6.2.0.tar.xz
+cd qemu-6.2.0
+rsync -r ../qemu-0/hw/3dfx ./hw/
+rsync -r ../qemu-1/hw/mesa ./hw/
+patch -p0 -i ../00-qemu620-mesa-glide.patch
+bash ../scripts/sign_commit
+mkdir ../build && cd ../build
+../qemu-6.2.0/configure && make -j
