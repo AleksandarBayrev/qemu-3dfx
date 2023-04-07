0f1a2fd741608208bfc282a2395d04c596605a4a
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2023-04-07 19:22:41 +0300

    Patch 7.2.0 script

--- build-7.2.0.sh
+++ build-7.2.0.sh
@@ -3,7 +3,7 @@ bash ./clean.sh
 wget --no-check-certificate https://download.qemu.org/qemu-7.2.0.tar.xz
 tar xfv qemu-7.2.0.tar.xz
 rm -rfv qemu-7.2.0.tar.xz
-cd qemu-6.2.0
+cd qemu-7.2.0
 rsync -r ../qemu-0/hw/3dfx ./hw/
 rsync -r ../qemu-1/hw/mesa ./hw/
 patch -p0 -i ../00-qemu720-mesa-glide.patch
