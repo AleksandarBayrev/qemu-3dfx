1485b51172181a47a1f3d05d566ff91dced2ee51
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2023-04-07 19:20:38 +0300

    Add 7.2.0 QEMU build script

---
+++ build-7.2.0.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+bash ./clean.sh
+wget https://download.qemu.org/qemu-7.2.0.tar.xz
+tar xfv qemu-7.2.0.tar.xz
+rm -rfv qemu-7.2.0.tar.xz
+cd qemu-6.2.0
+rsync -r ../qemu-0/hw/3dfx ./hw/
+rsync -r ../qemu-1/hw/mesa ./hw/
+patch -p0 -i ../00-qemu720-mesa-glide.patch
+bash ../scripts/sign_commit
+rm -rfv ../build && mkdir ../build && cd ../build
+../qemu-6.2.0/configure --target-list=i386-softmmu && make -j
