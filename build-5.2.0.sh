07c4bc43e95abbac4293e35ecc06fba842cabac0
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2023-04-08 17:01:36 +0300

    Fix small bugs in build scripts

--- build-5.2.0.sh
+++ build-5.2.0.sh
@@ -6,7 +6,7 @@ rm -rfv qemu-5.2.0.tar.xz
 cd qemu-5.2.0
 rsync -r ../qemu-0/hw/3dfx ./hw/
 rsync -r ../qemu-1/hw/mesa ./hw/
-patch -p0 -i ../01-qemu520-mesa-glide.patch
+patch -p0 -i ../02-qemu520-mesa-glide.patch
 bash ../scripts/sign_commit
 rm -rfv ../build && mkdir ../build && cd ../build
 ../qemu-5.2.0/configure --target-list=i386-softmmu && make -j
--- build-6.2.0.sh
+++ build-6.2.0.sh
@@ -6,7 +6,7 @@ rm -rfv qemu-6.2.0.tar.xz
 cd qemu-6.2.0
 rsync -r ../qemu-0/hw/3dfx ./hw/
 rsync -r ../qemu-1/hw/mesa ./hw/
-patch -p0 -i ../00-qemu620-mesa-glide.patch
+patch -p0 -i ../01-qemu620-mesa-glide.patch
 bash ../scripts/sign_commit
 rm -rfv ../build && mkdir ../build && cd ../build
 ../qemu-6.2.0/configure --target-list=i386-softmmu && make -j
